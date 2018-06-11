c3a303e8190ff8f0870056622f7f55967fe3069c
LucasLando <lucaslando1@gmail.com>
2018-06-10 23:43:56 -0300

    Adding codecov and coverage threshold values (#12)

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const isProduction = process.env.NODE_ENV === 'production'
 const port = isProduction ? process.env.PORT : 3001
 
 app.listen(port, () => {
+  /* eslint no-console: 0 */
   console.log(`Server running on port ${port}`)
 })
